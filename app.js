3c8b0c651cb6f4c8323ba4d9666a3382cb251f5c
Vinicius <vfonzar@gmail.com>
2025-02-26 17:46:21 -0300

    Adicionado nova funcionalidade ao sorteador

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 function sortear() {
-  let quantidade = document.getElementById('quantidade').value;
-  let de = document.getElementById('de').value;
-  let ate = document.getElementById('ate').value;
+  let quantidade = parseInt(document.getElementById('quantidade').value);
+  let de = parseInt(document.getElementById('de').value);
+  let ate = parseInt(document.getElementById('ate').value);
   
-  alert(`Quantidade: ${quantidade}`)
-  alert(`De: ${de}`)
-  alert(`Até: ${ate}`)
+  let sorteados = [];
+  let numero;
+
+  for (let i = 0; i < quantidade; i++) {
+    numero = obterNumeroAleatorio(de, ate);
+
+    while (sorteados.includes(numero)) {
+      numero = obterNumeroAleatorio(de, ate);
+    }
+
+    sorteados.push(numero);
+  }
+
+  let resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+  alterarStatusBotao();
   
+}
+
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  
+}
+
+function alterarStatusBotao() {
+  let botao = document.getElementById('btn-reiniciar');
+  if (botao.classList.contains('container__botao--desabilitado')) {
+    botao.classList.remove('container__botao--desabilitado');
+    botao.classList.add('container__botao');
+  } else {
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao--desabilitado');
+  }
 }
