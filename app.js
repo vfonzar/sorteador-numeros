6029e5e9db33d7d9ef3c06c739f085a51d14222b
Vinicius <vfonzar@gmail.com>
2025-02-26 14:50:50 -0300

    Adicionar arquivos do projeto

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear() {
+  let quantidade = document.getElementById('quantidade').value;
+  let de = document.getElementById('de').value;
+  let ate = document.getElementById('ate').value;
+  
+  alert(`Quantidade: ${quantidade}`)
+  alert(`De: ${de}`)
+  alert(`Até: ${ate}`)
+  
+}
